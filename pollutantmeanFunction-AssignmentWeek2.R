35b579d6e0a89071a23ab8e0ad39f3f7e2e62f7c
Veronica Rozendaal <47069137+rozenv1@users.noreply.github.com>
2019-05-27 08:14:32 -0700

    Add files via upload

--- pollutantmeanFunction-AssignmentWeek2.R
+++ pollutantmeanFunction-AssignmentWeek2.R
@@ -3,21 +3,12 @@ setwd("C:/Users/Office/Documents/RCourse/Week2/rprog_data_specdata/")
 
 ## The data is provided in a zip file, which contains 332 comma-separated-value (CSV) files containing pollution 
 ## monitoring data for fine particulate matter (PM) air pollution at 332 locations in the US. 
-## Each file contains data from a single monitor and the ID number for each monitor is contained in the file name. 
+## Each file, a data frame, contains data from a single monitor and the ID number for each monitor is contained in the file name. 
 ## For example, data for monitor 200 is contained in the file "200.csv". 
 ## Each file contains three variables:
 # Date: the date of the observation in YYYY-MM-DD format (year-month-day)
 # sulfate: the level of sulfate PM in the air on that date (measured in micrograms per cubic meter)
 # nitrate: the level of nitrate PM in the air on that date (measured in micrograms per cubic meter)
-
-## SAMPLE OUTPUT:
-# source("pollutantmeanFunction-AssignmentWeek2.R")
-# pollutantmean("specdata", "sulfate", 1:10)
-# [1] 4.064128
-# pollutantmean("specdata", "nitrate", 70:72)
-# [1] 1.706047
-# pollutantmean("specdata", "nitrate", 23)
-# [1] 1.280833
 
 ###############################################################################
 ## FUNCTION DESCRIPTION:
@@ -27,6 +18,17 @@ setwd("C:/Users/Office/Documents/RCourse/Week2/rprog_data_specdata/")
 ## Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the 
 ## directory specified in the 'directory' argument and returns the mean of the pollutant across all of the monitors, 
 ## ignoring any missing values coded as NA.
+##-----------------------------------------------------------------------------
+## SAMPLE OUTPUT:
+# source("pollutantmeanFunction-AssignmentWeek2.R")
+# pollutantmean("specdata", "sulfate", 1:10)
+# [1] 4.064128
+# pollutantmean("specdata", "nitrate", 70:72)
+# [1] 1.706047
+# pollutantmean("specdata", "nitrate", 23)
+# [1] 1.280833
+
+#############################################################
 
 pollutantmean<-function(directory, pollutant, id = 1:332){
         ## 'directory' is a character vector lenght 1 indicating the location of the CSV files
